336622970fc91cf53ea4e643947c74db488e3aee
FilipGaik <126327646+FilipGaik@users.noreply.github.com>
2024-02-16 00:12:07 +0100

    Login module added

--- index.js
+++ index.js
@@ -20,6 +20,38 @@ db.connect();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
+app.post('/login', async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    if(email != "" && password != "") {
+      const checkEmail = await db.query("SELECT email FROM users WHERE email = $1", [email]);
+      if(checkEmail.rows.length == 0) {
+        res.send("NOK");
+      } else {
+        const checkPassword = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+        //Comparing hashed password with user input password
+        bcrypt.compare(password, checkPassword.rows[0].password, (err, result) => {
+          if(err) {
+            throw new Error(err);
+          } else {
+            if(result) {
+              return res.json(checkPassword.rows[0].id);
+            } else {
+              return res.send("NOK password");
+            }
+          }
+        });
+      }
+    } else {
+      res.send("NOK");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 app.post('/register', async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
